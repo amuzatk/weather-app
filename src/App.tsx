49f30edbbbac23720fb164dbdec50869e02f7b0f
Kazmatics <amuzatk@gmail.com>
2023-03-22 22:17:57 +0100

    included name on app component

--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,7 @@ const App = () => {
     <div>
       {posts.map(post => (
         <div key={post.id}>
-          {/* <h2>{post.title}</h2> */}
+          <h2>{post.title}</h2>
           <p>{post.body}</p>
         </div>
       ))}

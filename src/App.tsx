cbfeb23aa6950a16c0fae865b1143078bb7e61fd
Kazmatics <amuzatk@gmail.com>
2023-03-22 21:09:23 +0100

    new branch

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,7 @@ const App = () => {
       {posts.map(post => (
         <div key={post.id}>
           <h2>{post.title}</h2>
-          <p>{post.body}</p>
+          {/* <p>{post.body}</p> */}
         </div>
       ))}
     </div>
